463b838d1bb72ea80a04873cce2efdb13f01cac4
Repall <repall.017@gmail.com>
2024-07-23 14:54:58 +0200

    Ajout de la modal fonctionnelle

--- FrontEnd/edit.js
+++ FrontEnd/edit.js
@@ -1,24 +1,213 @@
-// const result = require("./login")
+const modOff = document.getElementById("edit-bar");
+const modifyWorks = document.getElementById("edit");
+const modalAddWorks = document.getElementById("openModalAdd")
 
-const mod = document.getElementById("edit-bar")
 function editMod() {
-    const modify = document.getElementById("edit")
-    
-    modify.classList.toggle("hidden")
-    mod.classList.toggle("hidden")
+    modifyWorks.classList.toggle("hidden");
+    modOff.classList.toggle("hidden");
 
-    window.localStorage.removeItem("token")
+    window.localStorage.removeItem("token");
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    const token = window.localStorage.getItem("token");
 
-document.addEventListener("DOMContentLoaded", function() {
-    const token = window.localStorage.getItem("token")
-    
     if (token !== null) {
-        console.log("Token OK")
-        editMod()
+        console.log("Token OK");
+        editMod();
     }
+});
+
+
+const cancelButton = document.getElementById("closeModalDelete");
+const cancelButton2 = document.getElementById("closeModalAdd")
+
+const returnButton = document.getElementById("returnModal")
+
+const modalDelete = document.getElementById("modalDelete");
+const modalAdd = document.getElementById("modalAdd")
+
+function openCheck(modal) {
+    if (modal.open) {
+        console.log("Modal OUVERTE");
+    } else {
+        console.log("Modal FERMÉE");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    // AFFICHER UN PROJET
+    async function listWorksModal() {
+        const addListWorksModal = document.getElementById("listWorks")
+
+        addListWorksModal.innerHTML = ""
+
+        const works = JSON.parse(localStorage.getItem("works"))
+
+        works.forEach((project, index) => {
+            const projectElement = document.createElement("article")
+
+            const iconDelete = document.createElement("i")
+            iconDelete.id = "deleteWorks"
+            iconDelete.classList = "fa-solid fa-trash-can"
+            iconDelete.addEventListener("click", () => deleteImage(index))
+
+            const projectImage = document.createElement("img")
+            projectImage.src = project.imageUrl
+
+            projectElement.appendChild(iconDelete)
+            projectElement.appendChild(projectImage)
+            addListWorksModal.appendChild(projectElement)
+        })
+    }
+
+    // SUPPRIMER UN PROJET
+    async function deleteImage(index) {
+        try {
+            const works = JSON.parse(localStorage.getItem("works"))
+            const imageUrls = JSON.parse(localStorage.getItem("imageUrls"))
+
+            if (index < 0 || index >= works.length) {
+                console.error("Index de suppression invalide")
+                return
+            }
+
+            const workToDelete = works[index]
+
+            // SUPPRIMER DU LOCALSTORAGE
+            works.splice(index, 1)
+            imageUrls.splice(index, 1)
+            localStorage.setItem("works", JSON.stringify(works))
+            localStorage.setItem("imageUrls", JSON.stringify(imageUrls))
+
+            // SUPPRIMER DE L'API
+            if (workToDelete && workToDelete.id) {
+                await fetch(`http://localhost:5678/api/works/${workToDelete.id}`, {
+                    method: "DELETE",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${localStorage.getItem("token")}` // Ajouter le token dans les en-têtes
+                    }
+                })
+            }
+
+            listWorksModal()
+        } catch (error) {
+            console.error("Erreur lors de la suppression de l'image :", error)
+        }
+    }
+
+    // AJOUTER UN PROJET
+    document.getElementById("btnSubmit").addEventListener("click", async () => {
+        const title = document.getElementById("title").value
+        const category = document.getElementById("category").value
+        const imageInput = document.getElementById("image")
+        const file = imageInput.files[0]
+
+        if (!title || !category || !file) {
+            alert("Veuillez remplir tous les champs et sélectionner une image.")
+            return
+        }
+
+        const formData = new FormData()
+        formData.append("title", title)
+        formData.append("category", category)
+        formData.append("image", file)
+
+        try {
+            const response = await fetch("http://localhost:5678/api/works", {
+                method: "POST",
+                body: formData,
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem("token")}` // Ajouter le token d'authentification
+                }
+            })
+
+            if (!response.ok) {
+                throw new Error("Erreur lors de l'ajout du projet")
+            }
+
+            const newWork = await response.json()
+
+            const works = JSON.parse(localStorage.getItem("works"))
+            const imageUrls = JSON.parse(localStorage.getItem("imageUrls"))
+
+            works.push(newWork)
+            imageUrls.push(newWork.imageUrl)
+
+            localStorage.setItem("works", JSON.stringify(works))
+            localStorage.setItem("imageUrls", JSON.stringify(imageUrls))
+
+            document.getElementById("addProjectForm").reset()
+            listWorksModal()
+        } catch (error) {
+            console.error("Erreur lors de l'ajout du projet :", error)
+        }
+    })
+
+
+    // AFFICHER L'APERCU DE L'IMAGE
+    document.getElementById("image").addEventListener("change", (event) => {
+        const file = event.target.files[0]
+        const imagePreview = document.getElementById("imgPreview")
+        
+        if (file) {
+            const reader = new FileReader()
+
+            reader.onload = () => {
+                imagePreview.src = reader.result
+                imagePreview.style.display = "block"
+            }
+
+            reader.readAsDataURL(file)
+        } else {
+            imagePreview.src = ""
+            imagePreview.style.display = "none"
+        }
+    })
+
+    // AFFICHER LES PROJETS AU CHARGEMENT DE LA PAGE
+    listWorksModal()
 })
 
-mod.addEventListener("click", editMod)
 
+
+// OUVERTURE DE LA MODAL POUR DELETE LES PROJETS
+modifyWorks.addEventListener("click", () => {
+    modalDelete.showModal();
+    openCheck(modalDelete);
+});
+
+
+// CHANGEMENT DE MODAL
+modalAddWorks.addEventListener("click", () => {
+    modalDelete.close()
+    console.log("Changement de modal")
+    modalAdd.showModal()
+})
+returnButton.addEventListener("click", () => {
+    modalAdd.close()
+    console.log("Changement de modal")
+    modalDelete.showModal()
+})
+
+
+// FERMETURE DE LA MODAL
+window.addEventListener("click", () => {
+    if (event.target === modalDelete) {
+        modalDelete.close();
+        openCheck(modalDelete);
+    }
+});
+cancelButton.addEventListener("click", () => {
+    modalDelete.close();
+    openCheck(modalDelete);
+});
+cancelButton2.addEventListener("click", () => {
+    modalAdd.close();
+    openCheck(modalDelete);
+});
+
+
+
+modOff.addEventListener("click", editMod);
--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,16 +1,23 @@
-const buttonsContainer = document.querySelector("#portfolio .btn")
-const worksGallery = document.querySelector("#portfolio .gallery")
+const buttonsContainer = document.querySelector("#portfolio .btn");
+const worksGallery = document.querySelector("#portfolio .gallery");
 
 async function fetchWorks() {
     try {
-        const response = await fetch("http://localhost:5678/api/works")
-        const works = await response.json()
-        console.log("WORKS OK")
+        const response = await fetch("http://localhost:5678/api/works");
+        const works = await response.json();
+        console.log("WORKS OK");
+        // Extraire les URLs des images
+        const imageUrls = works.map((work) => work.imageUrl)
+
+        // Stocker les URLs dans le localStorage
+        localStorage.setItem("imageUrls", JSON.stringify(imageUrls))
+        localStorage.setItem('works', JSON.stringify(works));
+
         return works
     } catch (error) {
         console.error("Erreur lors de la récupération des projets :", error)
         return []
-    }  
+    }
 }
 async function fetchCategories() {
     try {
@@ -19,20 +26,19 @@ async function fetchCategories() {
         console.log("CATEGORIES OK")
         return categories
     } catch (error) {
-        console.error('Erreur lors de la récupération des catégories :', error)
+        console.error("Erreur lors de la récupération des catégories :", error)
         return []
     }
-};
+}
 
 function displayWorks(works) {
-    worksGallery.innerHTML = "" // Vider le conteneur
+    worksGallery.innerHTML = ""; // Vider le conteneur
 
-    works.forEach(project => {
+    works.forEach((project) => {
         const projectElement = document.createElement("figure")
 
-
         const projectTitle = document.createElement("figcaption")
-        projectTitle.textContent = project.title
+        projectTitle.textContent = project.title;
 
         const projectImage = document.createElement("img")
         projectImage.src = project.imageUrl
@@ -41,14 +47,15 @@ function displayWorks(works) {
         projectElement.appendChild(projectTitle)
         projectElement.appendChild(projectImage)
         worksGallery.appendChild(projectElement)
+
     });
-};
+}
 function filterWorks(works, category) {
     if (category === "all") {
-        return works
+        return works;
     }
-    return works.filter(project => project.categoryId === category)
-};
+    return works.filter((project) => project.categoryId === category)
+}
 // Fonction pour créer les boutons de filtre
 function createFilterButtons(categories) {
     buttonsContainer.innerHTML = "" // Vider le conteneur de boutons
@@ -61,30 +68,30 @@ function createFilterButtons(categories) {
     allButton.classList.add("btnSelect")
 
     // Créer des boutons pour chaque catégorie
-    categories.forEach(category => {
+    categories.forEach((category) => {
         const categoryButton = document.createElement("button")
         categoryButton.textContent = category.name
         categoryButton.addEventListener("click", () => clickFilterButtons(category.id, categoryButton))
         buttonsContainer.appendChild(categoryButton)
     });
-};
+}
 
 // Gestion des clics sur les boutons
 async function clickFilterButtons(category, clickButton) {
     const buttons = buttonsContainer.querySelectorAll("button")
-    buttons.forEach(button => button.classList.remove("btnSelect"))
+    buttons.forEach((button) => button.classList.remove("btnSelect"))
     clickButton.classList.add("btnSelect")
 
-    const works = await fetchWorks()
+    const works = await fetchWorks();
     const filteredWorks = filterWorks(works, category)
     displayWorks(filteredWorks)
-};
+}
 
 // Initialiser la page
 async function initializePage() {
     const categories = await fetchCategories()
     createFilterButtons(categories)
     clickFilterButtons("all", buttonsContainer.querySelector("button"))
-};
+}
 
-initializePage();
+initializePage()
